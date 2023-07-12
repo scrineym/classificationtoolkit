eb36e59ce34f836f7c8876626e71462e30d7d638
scrineym <michael.scriney@dcu.ie>
2023-07-12 14:43:01 +0100

    Changes to readme

---
+++ READEME.md
@@ -0,0 +1,11 @@
+# ClassificationToolkit
+
+A quick way of running multiple classifiers
+
+## Installation 
+
+To install run 
+
+```shell
+pip install git+
+```
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/scrineym/classificationtoolkit',
-    project_urls = {
+    project_urls={
         "Bug Tracker": "https://github.com/scrineym/classificationtoolkit/issues"
     },
     license='MIT',
